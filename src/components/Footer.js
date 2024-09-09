98c4453416924135ae37f9a9892ae3b0f9a94802
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-09-09 13:15:10 +0530

    feat: platforms in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,44 @@
-// src/components/Footer.js
+// import React from 'react';
+
+// const Footer = () => (
+//   <footer className="bg-gray-800 text-white p-4">
+//     <div className="container mx-auto text-center">
+//       <p>&copy; 2024 K H I Y A N A T D E O R I. All rights reserved.</p>
+//     </div>
+//   </footer>
+// );
+
+// export default Footer;
+
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => (
   <footer className="bg-gray-800 text-white p-4">
     <div className="container mx-auto text-center">
       <p>&copy; 2024 K H I Y A N A T D E O R I. All rights reserved.</p>
+      <div className="mt-4 flex justify-center space-x-4">
+        
+        <a 
+          href="https://github.com/Khiyanat-Deori" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="text-white hover:text-gray-400"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+
+        <a 
+          href="https://www.linkedin.com/in/Khiyanat-Deori" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="text-white hover:text-gray-400"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        
+      </div>
     </div>
   </footer>
 );
